f7cc865490ba042a9a90ec77064d8d4763581972
ch2isk4kos <ch2isk4kos@gmail.com>
2019-07-16 19:19:49 -0400

    added routing for signup && login && logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
     root 'welcome#home'
+
+    get 'signup', to: 'users#new', as: 'signup'
+    get 'login', to: 'sessions#new', as: 'login'
+    get 'logout', to: 'sessions#destroy', as: 'logout'
+
     resources :users, except: [:index]
+    resources :sessions, only: [:new, :create, :destroy]
 
 end
