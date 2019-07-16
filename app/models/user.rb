9bab25f143d8cc638ad6a81e8fa3e40687c41df2
ch2isk4kos <ch2isk4kos@gmail.com>
2019-07-16 16:52:32 -0400

    migrated users table && added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :username, :email, :password, presence: true
+    validates :username, :email, uniqueness: true
+    validates :password, confirmation: true
+    validates :password_confirmation, presence: true
 end
