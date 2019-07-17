cc9ed61b8ad003929ebb876e978e76265184e13e
ch2isk4kos <ch2isk4kos@gmail.com>
2019-07-17 15:54:05 -0400

    updated logout request action

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
 
     get 'signup', to: 'users#new'
     post 'signup', to: 'users#create'
+
     get 'login', to: 'sessions#new'
     post 'login', to: 'sessions#create'
-    get 'logout', to: 'sessions#destroy'
+
+    delete 'logout', to: 'sessions#destroy'
 
     resources :users, except: [:index]
     # resources :sessions, only: [:new, :create, :destroy]
