da7473e087ad148c51edf4d9ad6288a0376a9168
ch2isk4kos <ch2isk4kos@gmail.com>
2019-07-16 20:13:53 -0400

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
 
     root 'welcome#home'
 
-    get 'signup', to: 'users#new', as: 'signup'
-    get 'login', to: 'sessions#new', as: 'login'
-    get 'logout', to: 'sessions#destroy', as: 'logout'
+    get 'signup', to: 'users#new'
+    post 'signup', to: 'users#create'
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    get 'logout', to: 'sessions#destroy'
 
     resources :users, except: [:index]
-    resources :sessions, only: [:new, :create, :destroy]
+    # resources :sessions, only: [:new, :create, :destroy]
 
 end
