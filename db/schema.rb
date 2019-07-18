f024af1b8e02ae3716630680fb1490be2aec9ce4
ch2isk4kos <ch2isk4kos@gmail.com>
2019-07-17 21:12:44 -0400

    migrated sports table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_203202) do
+ActiveRecord::Schema.define(version: 2019_07_18_010943) do
+
+  create_table "sports", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
